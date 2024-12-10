8134435292244f52fc300c7f40550429384d1fa6
LeoAmaudruz <157134941+LeoAmaudruz@users.noreply.github.com>
2024-12-10 11:24:32 +0100

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -32,3 +32,6 @@ $app->match('/admin/user/{id}/edit', "Watson\Controller\AdminController::editUse
 
 // Remove a user
 $app->get('/admin/user/{id}/delete', "Watson\Controller\AdminController::deleteUserAction")->bind('admin_user_delete');
+
+// Flux RSS
+$app->get('/rss', "Watson\Controller\HomeController::rssAction")->bind('rss');
